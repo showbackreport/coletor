3d089080b319b4d05a0d9a9f8c7568a5d1644995
cmeinemorais <carlos.meinemorais@gmail.com>
2015-09-16 17:03:51 -0300

    Coletor com suporte a RPC

--- coletor.py
+++ coletor.py
@@ -1,54 +1,43 @@
-#!/usr/bin/python -O
+#!/usr/bin/python 
 # -*- coding: utf-8 -*-
+
+
 '''
  Pegar a saida o dump-acct e preparar para carga no SBR-L 
-
- Setembro/2015
-  - agora com suporte a XML/RPC
- 
- '''
-
-### Cabecalhos ###
-import syslog
-import sys
-### especial logos no syslog.
-def log_msg(msg,f):
-  name="Coletor_acct"
-  # EMERG=0   Alert=1 CRIT=2 ERR=3 WARN=4 NOTICE =5  INFO=6 DEBUG=7  
-  prior =  [syslog.LOG_EMERG,syslog.LOG_ALERT,syslog.LOG_CRIT,syslog.LOG_ERR,syslog.LOG_WARNING,syslog.LOG_NOTICE,syslog.LOG_INFO,syslog.LOG_DEBUG]  
-  #print prior[f],  name
-  syslog.openlog(name,prior[f])
-  syslog.syslog(prior[f],msg)
-  syslog.closelog()
-
-from datetime import datetime
-import sqlite3 as lite
-import shlex, subprocess
+'''
+import xmlrpclib
+global dbhost
+import time,sys
+start_time = time.time()
+#global server
 import socket
 ghost=socket.gethostname()
-'''
-# removido mysql
-try:
-  import MySQLdb
-except:
-   msg="ERRO: Sem suporte a mysql - MySQLdb"
-   f=3
-   log_msg(msg,f)
-   print msg 
-   sys.exit()
-'''
-
-
 # onde esta o psacct
+#print sys.argv
+if len(sys.argv) < 5 : 
+	print  """
+       formato correto:
+	   /home/coletor/bin/coletor.py /var/log/account/pacct.0  /home/coletor/bin/coletor.conf nodo01 localhost 56667
+          """
+	sys.exit()
+
+
 file_name = sys.argv[1]
-file_conf = sys.argv[2] 
+file_conf = sys.argv[2]
 # define o host
 try:
   HOST=sys.argv[3]
 except:
    HOST=ghost
 
+if ghost ==  "nodo01":
+   dbhost  = "localhost"
+#'http://localhost:56667'
+rpc_server = sys.argv[4]
+rpc_port   = sys.argv[5]
 
+   
+   
 import ConfigParser
 Config = ConfigParser.ConfigParser()
 Config.read(file_conf)
@@ -67,51 +56,65 @@ def ConfigSectionMap(section):
     return dict1
 
 
-global dbhost, dbuser, dbpass, dbbase, dump_acct
-
+global  dump_acct
+'''
 dbhost = ConfigSectionMap("mysql")['dbhost']
-dbport = ConfigSectionMap("mysql")['dbport']
+dbport = int(ConfigSectionMap("mysql")['dbport'])
 dbuser = ConfigSectionMap("mysql")['dbuser']
 dbpass = ConfigSectionMap("mysql")['dbpass']
 dbbase = ConfigSectionMap("mysql")['dbbase']
+'''
 dump_acct = ConfigSectionMap("dump")['acct']
 
-if ghost ==  "lpx3":
-   dbhost  = "localhost"
+
+### Cabecalhos ###
+import syslog
+import sys
+### especial logos no syslog.
+def log_msg(msg,f):
+  name="Coletor_acct"
+  # EMERG=0   Alert=1 CRIT=2 ERR=3 WARN=4 NOTICE =5  INFO=6 DEBUG=7  
+  prior =  [syslog.LOG_EMERG,syslog.LOG_ALERT,syslog.LOG_CRIT,syslog.LOG_ERR,syslog.LOG_WARNING,syslog.LOG_NOTICE,syslog.LOG_INFO,syslog.LOG_DEBUG]  
+  #print prior[f],  name
+  syslog.openlog(name,prior[f])
+  syslog.syslog(prior[f],msg)
+  syslog.closelog()
+
+from datetime import datetime
+import sqlite3 as lite
+import shlex, subprocess
 
 
 ## usa base em memorias, ganho tempo e nao deixo lixo
 con = lite.connect(':memory:')
 msg_coletor = []
 
-####  INICIO FUNCAO
+####  INICIO FUNCAO DE TRANSFERENCIAS ###
+
 
 def valid_host(HOST):
-  con=MySQLdb.connect(dbhost,dbport,dbuser,dbpass)
-  con.select_db(dbbase)
-  
-  query="""
-         SELECT ativo  FROM  host_desc  WHERE host = '%s'
-        """ % HOST
+	ServidorRpc="http://%s:%s" % (rpc_server, rpc_port)
+	proxy = xmlrpclib.ServerProxy(ServidorRpc, verbose=False)
+	return proxy.valida_host(HOST)
 
-  with con:
-    cur = con.cursor()
-    try:
-       cur.execute(query)
-       row=int(cur.rowcount)
-       result = 0
-    except:
-       result = 1
-  if int(row) == 0:  result = 1 
-  con.close()
-  return result
+def remote_into(Linhas,N,host):
+	ServidorRpc="http://%s:%s" % (rpc_server, rpc_port)
+	proxy = xmlrpclib.ServerProxy(ServidorRpc, verbose=False)
+	return proxy.remote_into(Linhas,N,host)
 
+def remote_local_users(Linhas,ghost):
+	ServidorRpc="http://%s:%s" % (rpc_server, rpc_port)
+	proxy = xmlrpclib.ServerProxy(ServidorRpc, verbose=False)
+	return proxy.remote_local_users(Linhas,ghost)
+
+### FIM DE TRANSFERENCIA	
+	
 
 def local_user():
  #getent  gshadow  ## quem tem shadow eh local!
  #  getent passwd $USER ;a
  # pega username
- command_line = "/usr/bin/getent gshadow" 
+ command_line = "sudo /usr/bin/getent shadow" 
  args = shlex.split(command_line)
  p = subprocess.Popen(args, stdout=subprocess.PIPE)
  result= p.communicate()[0]
@@ -143,45 +146,41 @@ def local_user():
 
  return Linhas
 
-
-def remote_loca_users(Linhas,ghost):
-  dbtable = "users"
-  import MySQLdb
-  con=MySQLdb.connect(dbhost,dbport,dbuser,dbpass)
-  con.select_db(dbbase)
-  b=len(Linhas)
-  count=0
-  with con:
-    cur = con.cursor()
-    for tulpas in Linhas:
-         tulpas=tulpas.split(",")
-         # "(%s ,%s, %s, '%s' )" % (uid, gid , Username,"grupo")
-         a ="INSERT INTO %s VALUES ( %s, %s , '%s' , '%s'  ) " %(dbtable,tulpas[0],tulpas[1],tulpas[2],tulpas[3])
-         #print a
-         try:
-             cur.execute(a)
-         except:
-            count+=1
-  con.close()
-  return b-count
+############  FUNCOES SQLITE 
 
 # prepara dados para a base em memoria
 def insql(line):
   content = []
   b=line.split("|")
-  command=b[0].strip()
-  time_user=b[1].strip()
-  time_system=b[2].strip()
-  time_effective=b[3].strip()
-  user_id=b[4].strip()
-  group_id=b[5].strip()
-  memory=b[6].strip()
-  if len(b) == 8 :
-     io="zerado"
-     time_date=b[7].strip()
+  if len(b) == 11 : 
+   # 0                1      2          3          4       5      6       7         ?8         9            10
+   # tput            |v3|     7.00|     0.00|     7.00|     0|     0|  1824.00|     0.00|    2825     2816|Wed Jul  1 15:47:56 2015
+   command=b[0].strip()
+   time_user=b[2].strip()
+   time_system=b[3].strip()
+   time_effective=b[4].strip()
+   user_id=b[5].strip()
+   group_id=b[6].strip()
+   memory=b[7].strip()
+   io=b[9].strip()
+   time_date=b[10].strip()
+
   else:
-    io=b[7].strip()
-    time_date=b[8].strip()
+   command=b[0].strip()
+   time_user=b[1].strip()
+   time_system=b[2].strip()
+   time_effective=b[3].strip()
+   user_id=b[4].strip()
+   group_id=b[5].strip()
+   memory=b[6].strip()
+   if len(b) == 8 :
+      io="zerado"
+      time_date=b[7].strip()
+   else:
+     io=b[7].strip()
+     time_date=b[8].strip()
+
+
   date_object = datetime.strptime(time_date, '%a %b %d %H:%M:%S %Y')
   #full_row='(NULL,"%s",%s,%s,%s,%s,%s,%s,"%s","%s")'  % (command,time_user,time_system,time_effective,user_id,group_id,memory,io,date_object)
   full_row='INSERT INTO acct VALUES("%s",%s,%s,%s,%s,%s,%s,"%s","%s")'  % (command,time_user,time_system,time_effective,user_id,group_id,memory,io,date_object)
@@ -201,8 +200,6 @@ def intodb(Inserts):
    except:
       result=1
    return result
- 
-
 
 def querys(n,host):
   #con = lite.connect('test.db')
@@ -221,40 +218,11 @@ def querys(n,host):
         linhas.append(row)
 
   return linhas
+################# FIM FUNCOES  SQLITE
 
-## parte seria: jogo os dados no banco de producao
-def remote_into(Linhas,N,host):
-  dbtable = ['acct_uid','acct_cmd']
-  con=MySQLdb.connect(dbhost,dbport,dbuser,dbpass)
-  con.select_db(dbbase)
-  b=len(Linhas)
-  count=0
-  with con:
-    cur = con.cursor()
-    for tulpas in Linhas:
-         if N == 0 :
-            a ="INSERT INTO %s VALUES ( '%s', %s , %s , %s  , %s , '%s' ) " %(dbtable[N],host,tulpas[0],tulpas[1],tulpas[2],tulpas[3],tulpas[4])
-         if N == 1 :
-            a ="INSERT INTO %s VALUES ( '%s', '%s' , %s , %s  , %s , %s , '%s')  " %(dbtable[N],host,tulpas[0],tulpas[1],tulpas[2],tulpas[3],tulpas[4],tulpas[5])
-         try:
-             cur.execute(a)
-         except: 
-            count+=1
-          
-  con.close()
-  return b-count
- 
-################# FIM AREA DE FUNCOES 
-# onde esta o psacct
-file_name = sys.argv[1]
-
-# define o host
-try:
-  HOST=sys.argv[3]
-except:
-   HOST=ghost
 
 # verifica se o host tem permissao para executar
+
 result=valid_host(HOST)
 if int(result) == 1:
   msg=HOST+" Falha, sem permissao para coleta"
@@ -263,8 +231,9 @@ if int(result) == 1:
   print msg
   sys.exit()
 
-
+# dumpacct para a memoria
 command_line = "%s %s" % (dump_acct,file_name)
+
 args = shlex.split(command_line)
 p = subprocess.Popen(args, stdout=subprocess.PIPE)
 result= p.communicate()[0]
@@ -275,6 +244,9 @@ for line in result.split("\n",):
    if not line == '':
      Inserts.append(insql(Dline))
 
+	 
+	 
+	 
 N_insert= str(len(Inserts))
 msg_user = "PACCT " + N_insert
 msg_coletor.append(msg_user)
@@ -289,7 +261,7 @@ if b == 1 :
   sys.exit()
 
 
-
+# insere usuarios na base 
 Efetivo_uid=querys(0,HOST)
 N_user_cons=str(len(Efetivo_uid))
 N=0
@@ -297,6 +269,7 @@ N_regU=str(remote_into(Efetivo_uid,N,HOST))
 msg_user = "Consolidados: Users %s/%s " %(N_user_cons,N_regU)
 msg_coletor.append(msg_user)
 
+# insere comandos na base
 Efetivo_comando=querys(1,HOST)
 N_cmd_cons=str(len(Efetivo_comando))
 N=1
@@ -306,16 +279,23 @@ msg_coletor.append(msg_user)
 
 # insere users local na base de dados.
 Balde=local_user()
-registro = str(remote_loca_users(Balde,HOST) )
+registro = str(remote_local_users(Balde,HOST) )
 N_Balde=str(len(Balde))
 msg_user = "Users_Locais %s/%s " %(N_Balde,registro)
 msg_coletor.append(msg_user)
 
 
-print "|".join(msg_coletor)
+tempo_execution= (time.time() - start_time)
+
+#print "|".join(msg_coletor)
 msg="|".join(msg_coletor)
+tempo_execution= (time.time() - start_time)
+msg_user = "Tempo Exec:%.2fs" %(tempo_execution)
+msg_coletor.append(msg_user)
+
+msg="|".join(msg_coletor)
+
 f=5
 log_msg(msg,f)
+print msg
 sys.exit()
-
-
